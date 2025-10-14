6c737db7a5675b85174c6a442490b80d40590fb4
henrikwoxneryd-glitch <henrikwoxneryd@gmail.com>
2025-10-14 13:46:01 +0200

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,7 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_Bolt_Database_URL: string;
-  readonly VITE_Bolt_Database_ANON_KEY: string;
+  readonly VITE_Bolt Database_ANON_KEY: string;
 }
 
 interface ImportMeta {
